1149f434b4fa295055f3b726bd6b3553719be612
johnolivercr <j.quesada.chaves@gmail.com>
2023-05-29 20:52:20 -0600

    Update App.java

--- Semana3/src/App.java
+++ Semana3/src/App.java
@@ -3,5 +3,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("Hello, World Prueba!");
+        System.out.println("Hello, World Prueba 2!");
     }  
 }
