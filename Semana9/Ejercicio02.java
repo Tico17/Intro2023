1c24a951f8b5fb3c106e8e7f4613cef2939a4f53
johnolivercr <j.quesada.chaves@gmail.com>
2023-07-10 21:10:32 -0600

    Update Ejercicio02.java

--- Semana9/Ejercicio02.java
+++ Semana9/Ejercicio02.java
@@ -2,6 +2,15 @@ package Semana9;
 
 public class Ejercicio02 {
     public static void main(String[] args) {
-        // .En un arreglo, mostrar los datos de un usuario ( nombre, edad y sexo).
+
+        // Paso 1 crear una clase persona.
+
+        // Paso 2 crear una instancia de la clase persona y asignar sus propiedades
+
+        // Paso 3 crear un arreglo de Personas
+
+        // Paso 4 asignar la persona a la primer posicion del arreglo
+
+        // Paso 5 mostrar la informacion de la posicion 0
     }
 }
