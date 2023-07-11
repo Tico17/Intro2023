350441640317288822192e06030a7dfcccebbfb5
johnolivercr <j.quesada.chaves@gmail.com>
2023-07-10 21:17:19 -0600

    uc

--- Semana9/Ejercicio03.java
+++ Semana9/Ejercicio03.java
@@ -3,5 +3,22 @@ package Semana9;
 public class Ejercicio03 {
     public static void main(String[] args){
         //Mostrar el promedio de notas de tres cursos
+
+        // Paso 1 crear arreglo de cursos
+
+        // Paso 2 crear arreglo de notas
+
+        // Paso 3 calcular el promedio: la suma de las notas dividido entre la cantidad de cursos
+
+        // Paso 4 mostrar la nota y el promedio de cada curso.
+
+        // Ejemplo de salida de informacion:
+        // Promedio de Cursos
+        // CURSO     NOTA
+        // -----     ----
+        // Diseño    85
+        // Ingles    80
+        // Mate      100
+        // El promedio es de: 88.33
     }
 }
--- Semana9/Ejercicio04.java
+++ Semana9/Ejercicio04.java
@@ -3,5 +3,9 @@ package Semana9;
 public class Ejercicio04 {
     public static void main(String[] args){
         //Ingresar por teclado 10 números en un arreglo y mostrar el numero mayor y el numero menor.
+
+        // 4 5 8 9 4 5 7 4 55
+
+        
     }
 }
