e3b3532fb24f2114c65260b04ff8cb76e764c8d2
johnolivercr <j.quesada.chaves@gmail.com>
2023-07-10 21:54:29 -0600

    Update Ejercicio03.java

--- Semana9/Ejercicio03.java
+++ Semana9/Ejercicio03.java
@@ -2,6 +2,7 @@ package Semana9;
 
 public class Ejercicio03 {
     public static void main(String[] args){
+
         //Mostrar el promedio de notas de tres cursos
 
         // Paso 1 crear arreglo de cursos
@@ -9,16 +10,29 @@ public class Ejercicio03 {
         // Paso 2 crear arreglo de notas
 
         // Paso 3 calcular el promedio: la suma de las notas dividido entre la cantidad de cursos
+        // int suma = notas[0] + notas[1] + notas[2];
+        // int promedio = suma/3;
 
+        // int acumuladoraSuma = 0;
+        // for
+        // acumuladoraSuma = acumuladoraSuma + notas[indice];
+        // for
+        
         // Paso 4 mostrar la nota y el promedio de cada curso.
 
         // Ejemplo de salida de informacion:
         // Promedio de Cursos
+
+        
         // CURSO     NOTA
         // -----     ----
+        
+        // FOR
         // Diseño    85
         // Ingles    80
         // Mate      100
+        // FOR
+
         // El promedio es de: 88.33
     }
 }
