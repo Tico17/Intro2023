6bfb474ee57f4c981bcdde97098f34bf8f3fe219
johnolivercr <j.quesada.chaves@gmail.com>
2023-07-10 21:01:50 -0600

    uc

--- Semana9/Ejercicio02.java
+++ Semana9/Ejercicio02.java
@@ -3,6 +3,5 @@ package Semana9;
 public class Ejercicio02 {
     public static void main(String[] args) {
         // .En un arreglo, mostrar los datos de un usuario ( nombre, edad y sexo).
-
     }
 }
